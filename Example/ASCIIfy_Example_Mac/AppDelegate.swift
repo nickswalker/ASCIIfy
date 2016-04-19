f7750b5b05c470ebd2ed9c6852edca6509985408
Nicholas Walker <nick@nickwalker.us>
2016-04-19 16:33:49 -0500

    Add Mac target

---
+++ Example/ASCIIfy_Example_Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ASCIIfy_Example_Mac
+//
+//  Created by Nick Walker on 4/19/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Example/ASCIIfy_Example_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ASCIIfy_Example_Mac
+//
+//  Created by Nick Walker on 4/19/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
