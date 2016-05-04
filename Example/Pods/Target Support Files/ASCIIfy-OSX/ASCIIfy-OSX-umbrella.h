135ceda95d9c1a8523c7f5f89c70bab786dfa22a
Nicholas Walker <nick@nickwalker.us>
2016-05-04 16:23:58 +0000

    Untrack example pods Add kdtree dependency

--- Example/Pods/Target Support Files/ASCIIfy-OSX/ASCIIfy-OSX-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double ASCIIfyVersionNumber;
-FOUNDATION_EXPORT const unsigned char ASCIIfyVersionString[];
-
--- Example/Pods/Target Support Files/Pods-ASCIIfy_OSX_Example/Pods-ASCIIfy_OSX_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_ASCIIfy_OSX_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ASCIIfy_OSX_ExampleVersionString[];
-
